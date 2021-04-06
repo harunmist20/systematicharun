47ff9d317498619589938a50d65f62870d4256b4
Harun-Or-Rashid <82055684+harunmist20@users.noreply.github.com>
2021-04-07 00:42:26 +0600

    Add files via upload

---
+++ index.html/js/main.js
@@ -0,0 +1,8 @@
+//Bx slider
+$(document).ready(function(){
+    $('.slider_size').bxSlider();
+    $('#navigation_menu').meanmenu({
+      meanmenuContainer: '#mobile_menu',
+      meanScreenWidth: 991,
+    });
+});
